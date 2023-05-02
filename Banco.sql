6ec96b4b58560441bd0dcb3a125d5bb0d73e0837
Mat-ALbuquerque <matalfesouza@gmail.com>
2023-05-02 09:34:24 -0300

    AULA 02/05

---
+++ Banco.sql
@@ -0,0 +1,6 @@
+-- Arquivo de criação de banco de dados
+
+DROP DATABASE projeto1 IF EXISTS;
+
+CREATE DATABASE projeto1;
+
---
+++ revisao.sql
@@ -0,0 +1,15 @@
+-- COMANDOS SQL SE DIVIDEM EM DUAS LINGUAGENS
+-- ---------------------------------------------
+-- DDL (Data Definition Languagem)
+-- => Comandos de definiçõa do banco de daddos
+-- Ex:  CREATE DATABASE NAME
+-- 		DROP DATABASE NAME
+-- 		CREATE TABLE NAME (COLUNAS)
+-- 		DROP TABLE NAME
+-- ---------------------------------------------
+-- DML (Data Manipulation Language)
+-- => Comandos de manipulação de dados
+-- Ex:  SELECT COLUNAS FROM TABELA
+-- 		INSERT INTO TABELA (COLUNAS) VALUES (VALORES)
+-- 		DELETE FROM TABELA
+-- 		UPDATE COLUNAS FROM TABELA
