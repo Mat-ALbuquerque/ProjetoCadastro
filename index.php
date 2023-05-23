134cf3537ba3d0b3f723fd5e93ea8b0559219aa7
Mat-ALbuquerque <matalfesouza@gmail.com>
2023-05-23 12:11:59 -0300

    aula 23/05 - login

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+    include('criasessao.php')
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,8 +11,21 @@
 </head>
 <body>
     <h3>Página Inicial - Projeto Cadastro IFSP</h3>
+    <?php
+        if(!empty($_SESSION['login']))
+        {
+            echo "<h4>Olá ". $_SESSION['login']['nome_usuario']."</h1>";
+            echo "<a href='logout.php'>Sair</a>";
+        }
+    ?>
     <hr>
     <ul>
+        <?php
+        if(empty($_SESSION['login']))
+        {
+            echo "<li><a href='login.html'>Login</a></li>";
+        }
+        ?>
         <li><a href="cadastro_usuario.html">Cadastrar</a></li>
         <li><a href="listar_usuarios.php">Listar</a></li>
     </ul>
